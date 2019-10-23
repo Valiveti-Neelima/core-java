95d3b4d9ce11866fa0348b4b0deb508bc7589733
Valiveti-Neelima <55785702+Valiveti-Neelima@users.noreply.github.com>
2019-10-23 12:30:29 +0530

    A2.java

---
+++ A2.java
@@ -0,0 +1,15 @@
+public class A2
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=1;
+		int b=2;
+		int c=3;
+		int d=4;
+		System.out.println(-a+b*c);
+		System.out.println((a+b)%c);
+		System.out.println(a+-b*c/d);
+		System.out.println(a+b/c*d-8%3);
+	}
+}
+
